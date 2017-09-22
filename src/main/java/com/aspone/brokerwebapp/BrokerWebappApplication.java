c4cb26a9d8150ba9df2d6a486cda9ea1f59dbfae
Mehmet Safa Ertekin <msefaertekin@gmail.com>
2017-09-22 20:55:14 +0300

    Initial commit

---
+++ src/main/java/com/aspone/brokerwebapp/BrokerWebappApplication.java
@@ -0,0 +1,12 @@
+package com.aspone.brokerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerWebappApplication.class, args);
+	}
+}
