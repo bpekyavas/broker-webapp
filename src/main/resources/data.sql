d1c3f6726f5e6d4631d5801d0b1d301a069806b2
Mehmet Safa Ertekin <msefaertekin@gmail.com>
2017-09-24 20:11:53 +0300

    #12: updated insert script to generate random rates for stocks

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
 INSERT INTO security (id, symbol, price, spread)
 VALUES
-	(1, 'GARAN.E', 10.00000000, 0.40000000),
-	(2, 'AKBNK.E', 8.00000000, 0.30000000),
-	(3, 'VAKBN.E', 14.40000000, 0.25000000),
-	(4, 'SAHOL.E', 3.00000000, 0.10000000),
-	(5, 'TCELL.E', 16.58000000, 0.23400000),
-	(6, 'PGSUS.E', 22.43000000, 0.02500000),
-	(7, 'TUPRS.E', 0.50000000, 0.00500000),
-	(8, 'NETAS.E', 100.52000000, 1.50000000),
-	(9, 'TSPOR.E', 61.50000000, 8.00000000),
-	(10, 'BJKAS.E', 1903.00000000, 100.00000000);
+	(1, 'GARAN.E', round((rand()*100), 2), round(rand(),2)),
+	(2, 'AKBNK.E', round((rand()*100), 2), round(rand(),2)),
+	(3, 'VAKBN.E', round((rand()*100), 2), round(rand(),2)),
+	(4, 'SAHOL.E', round((rand()*100), 2), round(rand(),2)),
+	(5, 'TCELL.E', round((rand()*100), 2), round(rand(),2)),
+	(6, 'PGSUS.E', round((rand()*100), 2), round(rand(),2)),
+	(7, 'TUPRS.E', round((rand()*100), 2), round(rand(),2)),
+	(8, 'NETAS.E', round((rand()*100), 2), round(rand(),2)),
+	(9, 'TSPOR.E', round((rand()*100), 2), round(rand(),2)),
+	(10, 'BJKAS.E', round((rand()*100), 2), round(rand(),2));
 
 INSERT INTO trader (id, name)
 VALUES
