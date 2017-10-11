876eaeb9bd331dbc431306a5bcbcd463ba971615
Mehmet Safa Ertekin <msefaertekin@gmail.com>
2017-10-11 22:44:50 +0300

    #27: added api info and upgraded ui version for springfox

--- src/main/java/com/mse/brokerwebapp/infrastructure/configuration/SwaggerConfig.java
+++ src/main/java/com/mse/brokerwebapp/infrastructure/configuration/SwaggerConfig.java
@@ -13,13 +13,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.regex("/api/.*"))
-          .build();
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/api/.*"))
+                .build();
     }
 
     private ApiInfo apiInfo() {
