5887d77873655b05a03844f0ca44850eb589b170
Mehmet Safa Ertekin <msefaertekin@gmail.com>
2017-10-11 22:57:08 +0300

    #28: overridden equals and hashcode implementation of current user according to sonarqube analysis.

--- src/main/java/com/mse/brokerwebapp/domain/vo/CurrentUser.java
+++ src/main/java/com/mse/brokerwebapp/domain/vo/CurrentUser.java
@@ -44,4 +44,24 @@ public class CurrentUser extends User{
     public void setUserType(UserType userType) {
         this.userType = userType;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        CurrentUser that = (CurrentUser) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return userType == that.userType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (userType != null ? userType.hashCode() : 0);
+        return result;
+    }
 }
